3e7331b03a2a5bfa2062e58d8237b052f599bc60
Northy <adarsharyanmuz@gmail.com>
2022-03-27 20:57:07 +0530

    added error handler

--- app.js
+++ app.js
@@ -16,13 +16,14 @@ const jobsRouter=require('./routes/jobs')
 // error handler
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
+const verifyToken = require('./middleware/authentication')
 
 app.use(express.json());
 // extra packages
 
 // routes
 app.use('/api/v1/auth',authRouter);
-app.use('/api/v1/jobs',jobsRouter)
+app.use('/api/v1/jobs',verifyToken,jobsRouter)
 
 
 app.use(notFoundMiddleware);
